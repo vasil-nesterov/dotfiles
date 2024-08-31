df5c2ecfeebec69736a96cdd67827008e80209d8
Vasily Nesterov <vasil@hey.com>
2024-08-31 14:30:32 +0200

    Add starship config, update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,24 @@
 # ~/.config/fish/config.fish
 
-set fish_greeting ""
+set fish_greeting "G'day!"
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
 export EDITOR="code --wait"
 export KUBE_EDITOR="code --wait"
 
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
+alias    l="ls -lA"
 alias   gs="git status"
 alias   be="bundle exec"
 alias   dc="docker-compose"
-alias    l="ls -lA"
-alias mine="open -na RubyMine.app --wait --args $argv"
 alias  cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+bind \eq beginning-of-line
+bind \ew backward-word
+bind \ee forward-word
+bind \er end-of-line
 
 starship init fish | source
 
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
 
---config.color_scheme = 'Sex Colors (terminal.sexy)'
 config.color_scheme = 'rose-pine'
-
 config.font = wezterm.font 'JetBrains Mono NL'
 config.font_size = 13.0
 
+
 return config
+
